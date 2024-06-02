270e9a3ad0bdb1dbf98095005ebb425ab95f1fae
RendiS10 <rendisutendi10@gmail.com>
2024-06-02 22:24:04 +0700

    Menambahkan routes dan pages

---
+++ src/scripts/views/pages/detailArtikel.js
@@ -0,0 +1,13 @@
+const DetailArtikel = {
+  async render() {
+    return `
+      <h2>Halaman Detail Artikel</h2>
+    `;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default DetailArtikel;
---
+++ src/scripts/views/pages/kontak.js
@@ -0,0 +1,13 @@
+const Kontak = {
+  async render() {
+    return `
+      <h2>Halaman Kontak </h2>
+    `;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default Kontak;
