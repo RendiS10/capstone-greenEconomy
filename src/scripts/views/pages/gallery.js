9997e7c2ec5211bd9a2944ebf492a453b94d03a2
Rendi Sutendi <rendisutendi10@gmail.com>
2024-06-07 07:36:59 +0700

    first build page gallery

--- src/scripts/views/pages/gallery.js
+++ src/scripts/views/pages/gallery.js
@@ -1,7 +1,61 @@
 const Gallery = {
   async render() {
     return `
-      <h2>Halaman Gallery</h2>
+    <article>
+    <section class="galeri">
+      <h2><strong>Galeri</strong></h2>
+      <div class="cards-project">
+        <div class="card">
+          <img
+            src="images/image_5.png"
+            alt=""
+          />
+          <div class="card-body">
+            <h3>Judul Kegiatan</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            src="images/image_5.png"
+            alt=""
+          />
+          <div class="card-body">
+            <h3>Judul Kegiatan</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            src="images/image_5.png"
+            alt=""
+          />
+          <div class="card-body">
+            <h3>Judul Kegiatan</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            src="images/image_5.png"
+            alt=""
+          />
+          <div class="card-body">
+            <h3>Judul Kegiatan</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit lor
+            </p>
+          </div>
+        </div>
+      </div>
+  </section>
+  </article>
     `;
   },
 
