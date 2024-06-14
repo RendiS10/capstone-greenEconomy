96e6c3acd0855035c5a53915933ea407a9e332e2
Rendi Sutendi <rendisutendi10@gmail.com>
2024-06-14 17:26:30 +0700

    add Page Join

--- src/scripts/views/pages/join.js
+++ src/scripts/views/pages/join.js
@@ -1,7 +1,69 @@
 const Join = {
   async render() {
     return `
-      <h2>Halaman Join</h2>
+  <article>
+      <section class="galeri">
+        <h2><strong>JOIN US</strong></h2>
+        <div class="card-join">
+          <span>
+            <p><i>    
+              ikuti event kami di kota anda !
+            </i></p>
+          </span>
+       
+          <div class="cards-project-join">
+            <div class="card">
+            <img
+              src="images/image_5.png"
+              alt=""
+            />
+          <div class="card-body">
+            <h3>Judul Kegiatan</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit
+          </p>
+            </div>
+          </div>
+          <div class="card">
+            <img
+              src="images/image_5.png"
+              alt=""
+            />
+          <div class="card-body">
+            <h3>Judul Kegiatan</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img
+          src="images/image_5.png"
+              alt=""
+            />
+            <div class="card-body">
+              <h3>Judul Kegiatan</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img
+              src="images/image_5.png"
+              alt=""
+            />
+            <div class="card-body">
+              <h3>Judul Kegiatan</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit lor
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+    </section>
+    </article>
     `;
   },
 
